bd04030186933a2ca50be460f361d7964555b6d9
mrhamburguesa17 <mrhamburguesa17@gmail.com>
2025-08-26 16:43:41 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',                      // genera el service worker y assets en /public
+  register: true,                      // registra automáticamente el SW
+  skipWaiting: true,                   // toma control apenas se instala
+  disable: process.env.NODE_ENV === 'development' // PWA solo en build prod
+})
+
+module.exports = withPWA({
+  reactStrictMode: true
+})
